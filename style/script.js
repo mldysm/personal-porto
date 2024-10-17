b704a9295510a4b22af3c34f9d07fab1117175ed
mldysm <melodysyalomifa@gmail.com>
2024-10-17 22:46:01 +0700

    pertama

---
+++ style/script.js
@@ -0,0 +1,18 @@
+function sayHi() {
+    alert('Hi! How are you?');
+  };
+
+  window.onload = function() {
+    document.getElementById('progress-html').style.width = '80%';
+    document.getElementById('progress-css').style.width = '80%';
+    document.getElementById('progress-php').style.width = '35%';
+    document.getElementById('progress-javascript').style.width = '50%';
+    document.getElementById('progress-laravel').style.width = '30%';
+    document.getElementById('progress-bootstrap').style.width = '75%';
+    document.getElementById('progress-java').style.width = '35%';
+    document.getElementById('progress-python').style.width = '45%';
+    document.getElementById('progress-odoo').style.width = '60%';
+  };
+  
+  
+  
